ebbd7b9dc1df7650460e2008fdc33758bc6d7d2c
laurealim <laureal.seu@gmail.com>
2019-06-12 14:29:52 +0600

    Add to cart

--- src/components/Ecom/Details.js
+++ src/components/Ecom/Details.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import { ProductConsumer } from "../../context";
 import { Link } from "react-router-dom";
-import {ButtonContainer}   from "./Button";
+import { ButtonContainer } from "./Button";
 
 class Details extends Component {
   render() {
     return (
-      <ProductConsumer> 
+      <ProductConsumer>
         {value => {
           console.log(value.detailProduct);
           const {
@@ -28,35 +28,39 @@ class Details extends Component {
               </div>
 
               <div className="row">
-                  <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                      <img src={img} className="img-fluid" alt="product"/>
-                  </div>
-                  <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
-                      <h2>model : {title}</h2>
-                      <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
-                          made by : <span className="text-uppercase">{company}</span>
-                      </h4>
-                      <h4 className="text-blue">
-                          <strong>
-                              price : <span>$</span> {price}
-                          </strong>
-                      </h4>
-                      <p className="text-capitalize font-weight-bold mt-3 mb-0">
-                          some info about product:
-                      </p>
-                      <p className="text-muted lead">{info}</p>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>model : {title}</h2>
+                  <h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+                    made by : <span className="text-uppercase">{company}</span>
+                  </h4>
+                  <h4 className="text-blue">
+                    <strong>
+                      price : <span>$</span> {price}
+                    </strong>
+                  </h4>
+                  <p className="text-capitalize font-weight-bold mt-3 mb-0">
+                    some info about product:
+                  </p>
+                  <p className="text-muted lead">{info}</p>
 
-                      <div className="">
-                          <Link to="/">
-                              <ButtonContainer>back to product</ButtonContainer>
-                          </Link>
-                          <ButtonContainer disabled={inCart?true:false} onClick={() =>{
-                              value.addToCart(id);
-                          }}>
-                              {inCart?"inCart":"add to cart"}
-                          </ButtonContainer>
-                      </div>
+                  <div className="">
+                    <Link to="/">
+                      <ButtonContainer>back to product</ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                      cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addCart(id);
+                      }}
+                    >
+                      {inCart ? "inCart" : "add to cart"}
+                    </ButtonContainer>
                   </div>
+                </div>
               </div>
             </div>
           );
